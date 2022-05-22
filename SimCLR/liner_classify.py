4344429e2ca2b189dc3d118604e16ebd61385b83
Hou Chengkai <96556513+JackHck@users.noreply.github.com>
2022-05-22 16:32:17 +0800

    Create liner_classify.py

---
+++ SimCLR/liner_classify.py
@@ -0,0 +1,313 @@
+import argparse
+import os
+import random
+import time
+import warnings
+import sys
+import math
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.multiprocessing as mp
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import os, sys
+sys.path.append(os.getcwd())
+from resnet  import SupConResNet,LinearClassifier,NormedLinear
+#import wide_resnet
+#from tensorboardX import SummaryWriter
+from sklearn.metrics import confusion_matrix
+from unbalance import IMBALANCECIFAR10, IMBALANCECIFAR100
+from utils import*
+from sample import ClassAwareSampler
+from loss import*
+
+parser = argparse.ArgumentParser(description='PyTorch Cifar Training')
+parser.add_argument('--dataset', default='cifar100', help='dataset setting')
+parser.add_argument('--imb_type', default="exp", type=str, help='imbalance type')
+parser.add_argument('--imb_factor', default=0.01, type=float, help='imbalance factor')
+parser.add_argument('--feat_dim', default=128, type=int, help='feature dimenmion for model')
+parser.add_argument('--train_rule', default='Reweight', type=str, help='data sampling strategy for train loader')
+parser.add_argument('--rand_number', default=0, type=int, help='fix random number for data sampling')
+parser.add_argument('--exp_str', default='0', type=str, help='number to indicate which experiment it is')
+parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+parser.add_argument('--epochs', default=45, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=128, type=int,
+                    metavar='N',
+                    help='mini-batch size')
+parser.add_argument('--lr_decay_rate', type=float, default=0.1,
+                        help='decay rate for learning rate')
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                    metavar='LR', help='initial learning rate', dest='lr')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--wd', '--weight-decay', default=0, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)',
+                    dest='weight_decay')
+parser.add_argument('-p', '--print-freq', default=10, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+parser.add_argument('--pretrained', dest='pretrained', action='store_true',
+                    help='use pre-trained model')
+parser.add_argument('--seed', default=None, type=int,
+                    help='seed for initializing training. ')
+parser.add_argument('--gpu', default=None, type=int,
+                    help='GPU id to use.')
+parser.add_argument('--root_log',type=str, default='log')
+parser.add_argument('--root_model', type=str, default='checkpoint')
+best_acc1 = 0
+
+
+
+def main():
+    args = parser.parse_args()
+    
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        cudnn.deterministic = True
+        warnings.warn('You have chosen to seed training. '
+                      'This will turn on the CUDNN deterministic setting, '
+                      'which can slow down your training considerably! '
+                      'You may see unexpected behavior when restarting '
+                      'from checkpoints.')
+
+    if args.gpu is not None:
+        warnings.warn('You have chosen a specific GPU. This will completely '
+                      'disable data parallelism.')
+
+    ngpus_per_node = torch.cuda.device_count()
+    main_worker(args.gpu, ngpus_per_node, args)
+def main_worker(gpu, ngpus_per_node, args):
+    global best_acc1
+    args.gpu = gpu
+    if args.gpu is not None:
+        print("Use GPU: {} for training".format(args.gpu))
+
+
+    num_classes = 100 if args.dataset == 'cifar100' else 10
+    model  =  SupConResNet(feat_dim=args.feat_dim)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = torch.nn.DataParallel(model).cuda()
+    PATH = 'ccl/cifra160/cifra_100_gr_2_epoch_999.pth'
+    model.load_state_dict(torch.load(PATH))
+    if  args.train_rule == 'Reweight' or args.train_rule == 'DRW' :
+        MLP = NormedLinear(64,num_classes)
+    else:
+        MLP = LinearClassifier(num_classes=num_classes)
+    MLP = torch.nn.DataParallel(MLP).cuda()   
+    optimizer = torch.optim.SGD(MLP.parameters(), args.lr,
+                                momentum=args.momentum,
+                                weight_decay=args.weight_decay)
+    cudnn.benchmark = True
+    
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    '''
+    transform_train = transforms.Compose([
+        transforms.RandomResizedCrop(size=32, scale=(0.2, 1.)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomApply([
+            transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)
+        ], p=0.8),
+        transforms.RandomGrayscale(p=0.2),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+     '''
+    transform_val = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    if args.dataset == 'cifar10':
+        train_dataset = IMBALANCECIFAR10(root='./dataset/data', imb_type=args.imb_type, imb_factor=args.imb_factor, rand_number=args.rand_number, train=True, download=True, transform=transform_train)
+        val_dataset = datasets.CIFAR10(root='./dataset/data', train=False, download=True, transform=transform_val)
+    elif args.dataset == 'cifar100':
+        train_dataset = IMBALANCECIFAR100(root='./dataset/data100', imb_type=args.imb_type, imb_factor=args.imb_factor, rand_number=args.rand_number, train=True, download=True, transform=transform_train)
+        val_dataset = datasets.CIFAR100(root='./dataset/data100', train=False, download=True, transform=transform_val)
+    else:
+        warnings.warn('Dataset is not listed')
+        return
+    cls_num_list = train_dataset.get_cls_num_list()
+    print('cls num list:')
+    print(cls_num_list)
+    args.cls_num_list = cls_num_list
+    if args.train_rule == 'DRW' or args.train_rule == 'CE' :
+        train_sampler = None
+    elif args.train_rule == 'kk':
+        train_sampler = ClassAwareSampler(train_dataset) 
+    elif args.train_rule == 'Reweight':
+        train_sampler = ImbalancedDatasetSampler(train_dataset)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
+        num_workers=args.workers, pin_memory=True, sampler=train_sampler)
+
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=100, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+    for epoch in range(args.start_epoch, args.epochs):
+        adjust_learning_rate(optimizer, epoch, args)
+        if args.train_rule == 'DRW' :
+            train_sampler = None
+            #idx = epoch // 120
+            idx=1
+            betas = [0, 0.9999]
+            effective_num = 1.0 - np.power(betas[idx], cls_num_list)
+            per_cls_weights = (1.0 - betas[idx]) / np.array(effective_num)
+            per_cls_weights = per_cls_weights / np.sum(per_cls_weights) * len(cls_num_list)
+            per_cls_weights = torch.FloatTensor(per_cls_weights).cuda(args.gpu)
+            criterion = LDAMLoss(cls_num_list=cls_num_list, max_m=0.5, s=30, weight=per_cls_weights).cuda(args.gpu)
+        elif args.train_rule == 'Reweight' :
+            criterion = LDAMLoss(cls_num_list=cls_num_list, max_m=0.5, s=30).cuda(args.gpu)
+        else:
+            criterion = nn.CrossEntropyLoss().cuda()
+        train(train_loader, model,MLP,criterion, optimizer, epoch, args,flag='train')
+        acc1 =validate(val_loader, model,MLP,criterion, epoch, args,flag='val')
+        best_acc1 = max(acc1, best_acc1)
+        print('acc/test_top1_best', best_acc1, epoch)
+        output_best = 'Best Prec@1: %.3f\n' % (best_acc1)
+        print(output_best)
+        
+        
+def train(train_loader, model,MLP,criterion, optimizer, epoch, args,flag='train'):
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    # switch to train mode
+    MLP.train()
+    model.eval()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    end = time.time()
+    for i, (input, target,cluster) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        
+        input = input.to(device)
+        target = target.to(device)
+        with torch.no_grad():
+            features = model.module.encoder(input)
+            if  isinstance(features,tuple):
+                out,features = features
+            else:
+                features = features
+        output = MLP(features.detach())
+        loss = criterion(output, target)
+      #  loss = loss_c + 0.1*loss_d
+     
+        # measure accuracy and record loss       
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), input.size(0))
+        top1.update(acc1[0], input.size(0))
+        top5.update(acc5[0], input.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+        torch.cuda.empty_cache() 
+    output = ('{flag} Results: Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f} Loss {loss.avg:.5f}'
+                .format(flag=flag, top1=top1, top5=top5, loss=losses))
+    print(output)
+    torch.cuda.empty_cache()    
+
+
+def validate(val_loader, model,MLP,criterion, epoch, args,flag='val'):
+    batch_time = AverageMeter('Time', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    
+    # switch to evaluate mode
+    model.eval()
+    MLP.eval()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    all_preds = []
+    all_targets = []
+    with torch.no_grad():
+        end = time.time()
+        for i, (input, target) in enumerate(val_loader):
+            input = input.to(device)
+            target = target.to(device)
+            # compute output
+            features = model.module.encoder(input)
+            if  isinstance(features,tuple):
+                out,features = features
+            else:
+                features = features
+            output = MLP(features)
+            loss = criterion(output,target)
+
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), input.size(0))
+            top1.update(acc1[0], input.size(0))
+            top5.update(acc5[0], input.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+            #print(output)
+            _, pred = torch.max(output, 1)
+            all_preds.extend(pred.cpu().numpy())
+            all_targets.extend(target.cpu().numpy())
+        cf = confusion_matrix(all_targets, all_preds).astype(float)
+        cls_cnt = cf.sum(axis=1)
+        cls_hit = np.diag(cf)
+        cls_acc = cls_hit / cls_cnt
+        output = ('{flag} Results: Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f} Loss {loss.avg:.5f}'
+                .format(flag=flag, top1=top1, top5=top5, loss=losses))
+        out_cls_acc = '%s Class Accuracy: %s'%(flag,(np.array2string(cls_acc, separator=',', formatter={'float_kind':lambda x: "%.3f" % x})))
+        print(output)
+        print(out_cls_acc)
+        
+
+    return top1.avg
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    epoch = epoch + 1
+    if args.train_rule == 'kk' or args.train_rule == 'Reweight':
+        if epoch <= 5:
+            lr = args.lr * epoch / 5
+        else:
+            lr_min = 0
+            lr_max = args.lr
+            lr = lr_min + 0.5 * (lr_max - lr_min) * (1 + math.cos((epoch-5) / (args.epochs-5) * 3.1415926535))
+    else:
+        if epoch <= 5:
+            lr = args.lr * epoch / 5
+        elif epoch > 180:
+             lr = args.lr * 0.01
+        elif epoch > 140:
+             lr = args.lr * 0.1
+        elif epoch > 190:
+             lr = args.lr * 0.001
+        else:
+             lr = args.lr
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+
+if __name__ == '__main__':
+    main()
