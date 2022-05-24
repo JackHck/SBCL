26961f3b41bb5d8a59954b5df933323eb316b8bd
Hou Chengkai <96556513+JackHck@users.noreply.github.com>
2022-05-24 21:34:08 +0800

    Update loss.py

--- moco/loss.py
+++ moco/loss.py
@@ -99,77 +99,27 @@ class SupConLoss_rank(nn.Module):
         self.ranking_temperature = ranking_temperature
         self.grama = grama
         self.K = K
-    def forward(self, logits,im_labels,queue_label,im_cluster,queue_cluster):
+    def forward(self, logits,im_label,queue_label,im_cluster,queue_cluster):
 
-        im_labels = im_labels.contiguous().view(-1, 1)
-        label_mask = torch.eq(im_labels, queue_labels).float()
+        im_label = im_label.contiguous().view(-1, 1)
+        label_mask = torch.eq(im_label, queue_label).float()
         
         im_cluster = im_cluster.contiguous().view(-1, 1)
         cluster_mask = torch.eq(im_cluster, queue_cluster).float()
-        cluster_mask = torch.cat([torch.ones([cluster_mask.shape[0], 1]).cuda(), cluster_mask], dim=1)
+        cluster_mask_com = torch.cat([torch.ones([cluster_mask.shape[0], 1]).cuda(), cluster_mask], dim=1)
         logits_cluster = logits/self.temperature
         log_cluster_prob = F.normalize(logits_cluster.exp(), dim=1, p=1).log()
-        loss_cluster = - torch.sum((cluster_mask * log_cluster_prob).sum(1) / cluster_mask.sum(1)) / cluster_mask.shape[0]
+        loss_cluster = - torch.sum((cluster_mask_com * log_cluster_prob).sum(1) / cluster_mask_com.sum(1)) / cluster_mask_com.shape[0]
 
         Bool = ~cluster_mask.bool()
         Inverse_cluster =Bool.float()
         label_mask = Inverse_cluster * label_mask
-        label_mask = torch.cat([torch.ones([label.shape[0], 1]).cuda(), label_mask], dim=1)
+        label_mask = torch.cat([torch.ones([label_mask.shape[0], 1]).cuda(), label_mask], dim=1)
         Inverse_cluster = torch.cat([torch.ones([Inverse_cluster.shape[0], 1]).cuda(), Inverse_cluster], dim=1)
         logits_label = logits/self.ranking_temperature
         log_inverse_cluster = torch.exp(logits_label) * Inverse_cluster
         log_label_prob = logits_label - torch.log(log_inverse_cluster .sum(1, keepdim=True))
         loss_label = - torch.sum((label_mask* log_label_prob).sum(1) / label_mask.sum(1)) / label_mask.shape[0]
-        loss =loss_cluster + self.gramma*loss_label
+        loss =loss_cluster + self.grama*loss_label
 
         return loss   
-
-class SupConLoss_warm(nn.Module):
-    def __init__(self,K,gramma=0.2, temperature=0.07, 
-                 base_temperature=None):
-        super(SupConLoss_warm, self).__init__()
-        self.temperature = temperature
-        self.base_temperature = temperature if base_temperature is None else base_temperature
-        self.K = K
-        self.gramma = gramma
-    def forward(self, features, labels, mask=None):
-        device = (torch.device('cuda')
-                  if features.is_cuda
-                  else torch.device('cpu'))
-
-        ss = features.shape[0]
-        batch_size = (features.shape[0] - self.K ) // 2
-
-        labels = labels.contiguous().view(-1, 1)
-        mask = torch.eq(labels[:batch_size], labels.T).float().to(device)
-
-        # compute logits
-        anchor_dot_contrast = torch.div(
-            torch.matmul(features[:batch_size], features.T),
-            self.temperature)
-
-        # for numerical stability
-        logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
-        logits = anchor_dot_contrast - logits_max.detach()
-
-        # mask-out self-contrast cases
-        logits_mask = torch.scatter(
-            torch.ones_like(mask),
-            1,
-            torch.arange(batch_size).view(-1, 1).to(device),
-            0
-        )
-
-        mask = mask * logits_mask 
-
-        exp_log_logits = torch.exp(logits) * logits_mask
-        log_prob = logits - torch.log(exp_log_logits .sum(1, keepdim=True))
-        # compute mean of log-likelihood over positive
-        mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
-        
-        # loss
-      
-        loss =-(self.temperature / self.base_temperature) * mean_log_prob_pos
-        loss = loss.mean()
-
-        return loss
